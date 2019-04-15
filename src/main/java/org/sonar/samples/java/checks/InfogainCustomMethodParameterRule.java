aa0a99873a3dc3b1b496bb9f52d2cffdb0c19fa8
rudhrainfogain <rudhra.koul@infogain.com>
2019-04-15 16:54:26 +0530

    added custom rule

--- src/main/java/org/sonar/samples/java/checks/InfogainCustomMethodParameterRule.java
+++ src/main/java/org/sonar/samples/java/checks/InfogainCustomMethodParameterRule.java
@@ -2,15 +2,60 @@ package org.sonar.samples.java.checks;
 
 import java.util.List;
 
+import org.sonar.check.Priority;
+import org.sonar.check.Rule;
 import org.sonar.plugins.java.api.IssuableSubscriptionVisitor;
-import org.sonar.plugins.java.api.tree.Tree.Kind;
+import org.sonar.plugins.java.api.semantic.Symbol.MethodSymbol;
+import org.sonar.plugins.java.api.semantic.Type;
+import org.sonar.plugins.java.api.tree.MethodTree;
+import org.sonar.plugins.java.api.tree.Tree;
 
-public class InfogainCustomMethodParameterRule  extends IssuableSubscriptionVisitor {
+import com.google.common.collect.ImmutableList;
 
+@Rule(key = "InfogainRecomendationAvoidMethodWithSameTypeInArgument",
+description = "For a method having a single parameter, the types of its return value and its parameter should never be the same.",
+priority = Priority.CRITICAL,
+tags = {"bug"})
+	
+/**
+ * To use subsctiption visitor, just extend the IssuableSubscriptionVisitor.
+ * 
+ * @author Rudhra Koul
+ */
+public class InfogainCustomMethodParameterRule extends IssuableSubscriptionVisitor {
+
+	private final String MESSAGE="Infogain Recomends Avoiding Method With Same return Type and Argument";
+	/**
+	 * @author Rudhra Koul This method is used to identify the type of element like
+	 *         method ,constructor,catch block etc to apply the rule on
+	 */
 	@Override
-	public List<Kind> nodesToVisit() {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Tree.Kind> nodesToVisit() {
+		// Register to the kind of nodes you want to be called upon visit.
+		return ImmutableList.of(Tree.Kind.METHOD);
 	}
 
+	/**
+	 * @author Rudhra Koul This is the actual rule that is implemented
+	 */
+	@Override
+	public void visitNode(Tree tree) {
+		// Cast the node to the correct type :
+		// in this case we registered only to one kind so we will only receive
+		// MethodTree @see Tree.Kind enum to know about which type you can
+		// cast depending on Kind.
+		MethodTree methodTree = (MethodTree) tree;
+		// Retrieve symbol of method.
+		MethodSymbol methodSymbol = methodTree.symbol();
+		Type returnType = methodSymbol.returnType().type();
+		// Check method has only one argument.
+		if (methodSymbol.parameterTypes().size() == 1) {
+			Type argType = methodSymbol.parameterTypes().get(0);
+			// Verify argument type is same as return type.
+			if (argType.is(returnType.fullyQualifiedName())) {
+				// raise an issue on this node of the SyntaxTree
+				reportIssue(tree, MESSAGE);
+			}
+		}
+	}
 }
--- src/test/files/InfogainCustomMethodParameterRuleTestData.java
+++ src/test/files/InfogainCustomMethodParameterRuleTestData.java
@@ -5,5 +5,11 @@
  */
 class A {
 
-  
+  int foo() {}
+  int foo(int a) {} // Noncompliant {{Infogain Recomends Avoiding Method With Same return Type and Argument}}
+  int foo(int a, int b) {}
+
+  Object foo(Object a){} // Noncompliant {{Infogain Recomends Avoiding Method With Same return Type and Argument}}
+  String foo(String a){} // Noncompliant {{Infogain Recomends Avoiding Method With Same return Type and Argument}}
+  String foo(Object a){}
 }
